98949cc7092ab53124e4d7dbdda73ed2aaf91295
Dan Diodati <dandiodati@gmail.com>
2026-04-10 11:34:04 -0700

    Removed unused landing constant

--- custom_components/openid/views.py
+++ custom_components/openid/views.py
@@ -29,7 +29,6 @@ from homeassistant.util import slugify
 from .const import (
     CONF_AUTHORIZE_URL,
     CONF_BLOCK_LOGIN,
-    CONF_LANDING_URL,
     CONF_CONSENT_PROMPT,
     CONF_CREATE_USER,
     CONF_LOGOUT_URL,
